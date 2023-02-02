a3f64e1cc719c6659f3f265c3607a19336693183
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-02 10:08:24 -0800

    Commit week 4a factory pseudocode.

---
+++ factories.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# Factorybot
+
+# build() -- makes a temp object
+# create() -- makes an object and persists it to the db
+
+Rspec.describe Region, type: model do
+  # test
+end
