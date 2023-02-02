9909b1c68a5db30df439b3149af895a1162db96b
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-02 11:18:23 -0800

    Add different types of exceptions.

--- exceptions.rb
+++ exceptions.rb
@@ -1,6 +1,30 @@
+# basic example
 begin
   puts 1/0
 rescue
   puts "Hey, bad news. You can't divide by zero"
 end
-puts "Program will never get here"
+
+# access exception object
+begin
+  puts 1/0
+rescue => e
+  puts "Hey, bad news. You can't divide by zero"
+  puts "Here are the details of the exception:"
+  puts "Classname: #{e.class.name}"
+  puts "Message: #{e.message}"
+  puts "Backtrace: #{e.backtrace}"
+end
+
+# explicitly catch certain kinds of exceptions (with default)
+begin
+  puts 1/0
+rescue ZeroDivisionError => e
+  puts "Hey, bad news. You can't divide by zero"
+  puts "Here are the details of the exception:"
+  puts "Classname: #{e.class.name}"
+  puts "Message: #{e.message}"
+  puts "Backtrace: #{e.backtrace}"
+rescue
+  puts "I don't know what kind of error you encountered"
+end
